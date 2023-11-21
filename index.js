1bea89885134a62dd781cab23f1ad5769162a672
FarzadVav <vav.progg.ir@gmail.com>
2023-11-21 05:53:56 -0800

    add eyeDropper to js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log(123);
+document.querySelector(".pick").addEventListener("click", () => {
+  const resultElement = document.querySelector(".result");
+
+  if (!window.EyeDropper) {
+    resultElement.textContent =
+      "Your browser does not support the EyeDropper API";
+    return;
+  }
+
+  const eyeDropper = new EyeDropper();
+
+  eyeDropper
+    .open()
+    .then((result) => {
+      resultElement.textContent = result.sRGBHex;
+      resultElement.style.backgroundColor = result.sRGBHex;
+      document.body.style.borderColor = `${result.sRGBHex}45`
+    })
+    .catch((e) => {
+      resultElement.textContent = e;
+    });
+});
